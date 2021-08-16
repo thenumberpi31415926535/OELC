25ec8d6e922550da7704d637775deb2561337a90
Ko-Lin <kochang.chu@gmail.com>
2021-08-17 00:16:43 +0800

    Update SyncTalks.ps1

--- SyncTalks.ps1
+++ SyncTalks.ps1
@@ -197,8 +197,10 @@ function ExtractDateTimeFromString
     (
         [psobject]$String
     )
+    $culture = New-Object System.Globalization.CultureInfo("zh-TW", $false);
+
     $date = [System.DateTimeOffset]::MinValue
-    if([System.DateTimeOffset]::TryParseExact($String, 'MM/dd/yyyy H:mm:ss', [System.Globalization.CultureInfo]:: InvariantCulture, [System.Globalization.DateTimeStyles]::None, [ref]$date))
+    if([System.DateTimeOffset]::TryParseExact($String, 'MM/dd/yyyy H:mm:ss', $culture, [System.Globalization.DateTimeStyles]::None, [ref]$date))
     {
         return $date
     }
